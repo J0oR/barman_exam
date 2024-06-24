36035b17549c0098a1bfab291a0ed77588fe2500
Giovanni Ruocco <giovanniruocco1@gmail.com>
2024-06-24 12:53:55 +0200

    home load fix

--- main.js
+++ main.js
@@ -2,71 +2,65 @@ const app = Vue.createApp({
   /********************************* DATA *********************************/
   data() {
     return {
+      drinkData: [], // Variable to store all drink data
       drink: '',
       show_drink: false,
       show_recipe: false
     }
   },
-  mounted() {
-    // Add a visibility change event listener
-    document.addEventListener("visibilitychange", this.handleVisibilityChange);
-  },
-
   /********************************* METHODS *********************************/
   methods: {
-    async selectDrink(){
-        const files = [
-          './assets/data/build-a.json',
-          './assets/data/build-b.json',
-          './assets/data/dryshake.json',
-          './assets/data/muddler.json',
-          './assets/data/stir-strain.json',
-          './assets/data/shake-strain.json'
-        ];
-    
-        try {
-          // Fetch all files and combine their contents into a single array
-          const allElements = [];
-          for (const file of files) {
-            const response = await fetch(file);
-            const elements = await response.json();
-            allElements.push(...elements);
-          }
-    
-          // Select a random element from the combined array
-          const randomIndex = Math.floor(Math.random() * allElements.length);
-          this.drink = allElements[randomIndex];
-          this.show_drink = true;
-          this.show_recipe = false;
-        } catch (error) {
-          console.error('Error fetching elements:', error);
+    async fetchDrinkData() {
+      const files = [
+        './assets/data/build-a.json',
+        './assets/data/build-b.json',
+        './assets/data/dryshake.json',
+        './assets/data/muddler.json',
+        './assets/data/stir-strain.json',
+        './assets/data/shake-strain.json'
+      ];
+
+      try {
+        // Fetch all files and combine their contents into a single array
+        const allElements = [];
+        for (const file of files) {
+          const response = await fetch(file);
+          const elements = await response.json();
+          allElements.push(...elements);
         }
-      },
-      alternate_cards(){
-        this.show_drink = !this.show_drink;
-        this.show_recipe = !this.show_drink;
+        
+        // Store all drink data in drinkData
+        this.drinkData = allElements;
+      } catch (error) {
+        console.error('Error fetching elements:', error);
       }
     },
-  /******************************** COMPUTED PROPERTIES *****************************/
-  computed: {
-    timerDisplay() {
-      const minutes = this.minutes < 10 ? '0' + this.minutes : this.minutes;
-      const seconds = this.seconds < 10 ? '0' + this.seconds : this.seconds;
-      return minutes + ':' + seconds;
+    selectDrink() {
+      // Select a random drink from drinkData
+      const randomIndex = Math.floor(Math.random() * this.drinkData.length);
+      this.drink = this.drinkData[randomIndex];
+      this.show_drink = true;
+      this.show_recipe = false;
+    },
+    alternate_cards() {
+      this.show_drink = !this.show_drink;
+      this.show_recipe = !this.show_drink;
     }
   },
-  /********************************* WATCHERS  *********************************/
-  watch: {
-    /* handles break/session ending */
-    seconds(currentValue) {
-      // timer hass reached 0
-      if (currentValue === -1 && this.minutes === 0) {
-        this.playSound();
-        // switch between session and rest modes when timer reaches zero
-        this.timerType = (this.timerType === 'session') ? 'rest' : 'session';
-        this.timerType === 'rest' ? this.startRest() : this.startCountdown(mode = 'onGoing');
+  /******************************** COMPUTED PROPERTIES *****************************/
+  computed: {
+    adjustedImagePath() {
+      if (this.drink && this.drink.img && this.drink.img.startsWith('../')) {
+        return this.drink.img.substring(1);  // Remove the first dot (.)
+      } else {
+        return this.drink.img;  // Return the image path as is
       }
     }
+  },
+  /********************************* LIFECYCLE HOOKS *****************************/
+  created() {
+    // Fetch drink data before mounting
+    this.fetchDrinkData();
   }
 });
 
